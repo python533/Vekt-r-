182f6ebff34032e5068669305d67512588831925
python533 <143389231+python533@users.noreply.github.com>
2024-04-05 09:30:57 +0300

    Calculate Vektör
    Python Vektör

---
+++ vektör.py
@@ -0,0 +1,16 @@
+Vektör=input("Hesaplanacak Bir Vektör Girin:")
+
+
+while True:
+    if Vektör >10:
+        print("Bu Mesafeye Gidilebilir")
+    elif Vektör <10:
+        print("Bu Mesafeye Gidilemez!")
+
+    else:
+        print("Geçerli Bir Vektör Giriniz!")
+
+
+
+
+
